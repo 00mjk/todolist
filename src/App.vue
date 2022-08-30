a559ecd3f1ffaf34c08194aece98056843377f7a
Bxnq <86614234+Bxnq@users.noreply.github.com>
2022-08-30 15:43:57 +0200

    Init Files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,151 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todos">
+    <h1>Deine Todos</h1>
+    <h2 v-if="openTodos.length > 0">Offene Todos: {{ openTodos.length }}</h2>
+    <h2 v-else>Keine Offene Todos</h2>
+  </div>
+
+  <div class="todoSection">
+    <div class="addTodo">
+      <input @keydown.enter="addTodo" type="text" v-model="newTodo">
+      <button @click="addTodo">Add</button>
+    </div>
+
+    <hr v-if="todos.length > 0" color="515151">
+
+    <div v-for="(todo, index) in todos" :key="todo.todoName">
+      <ToDo :todoprop="todo" :todoindex="index"
+      @toggledone-index="toggleDone"
+      @deletetodo-index="deleteTodoItem"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDo.vue';
 
 export default {
   name: 'App',
+
+  data() {
+    return {
+      newTodo: "",
+      todos: [
+        {
+          todoName: "TODONAME", done: true
+        }
+      ],
+    }
+  },
+  methods: {
+    toggleDone(index) {
+        this.todos[index].done = !this.todos[index].done;
+        this.storeTodos();
+    },
+    deleteTodoItem(index) {
+        this.todos.splice(index,1);
+        this.storeTodos();
+    },
+    addTodo() {
+      if(this.newTodo.trim() === "") {
+        return;
+      }
+      this.todos.push({ todoName: this.newTodo, done: false});
+      this.storeTodos();
+      this.newTodo = "";
+    },
+    storeTodos() {
+      localStorage.setItem("todos", JSON.stringify(this.openTodos));
+    }
+  },
+  mounted() {
+    let data = localStorage.getItem("todos");
+
+    if(data !== "" && data !== null) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  },
+  computed: {
+    openTodos() {
+      const openTodos = this.todos.filter((todo) => {
+        return !todo.done;
+      });
+      return openTodos;
+    },
+    closedTodos() {
+      const closedTodos = this.todos.filter((todo) => {
+        return todo.done;
+      });
+      return closedTodos;
+    }
+  },
   components: {
-    HelloWorld
+    ToDo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  margin: 0;
+}
+
+.addTodo {
+  display: flex;
+  justify-content: space-between;
+}
+
+.addTodo input {
+  width: 100vh;
+  padding: 20px;
+  border: 0;
+  border-radius: 10px;
+  background-color: rgb(0,0,0, 0.5);
+  color: white;
+  font-size: large;
+  
+}
+
+.addTodo button {
+  margin-left: 5%;
+  width: 30vh;
+  border-radius: 10px;
+  border: 0;
+  color: white;
+  font-size: larger;
+  background-color: rgb(0,0,0,0.5);
+}
+
+.addTodo button:hover {
+  background-color: rgb(255, 255, 255, 0.1);
+}
+
+
+
+html {
+  background-color: var(--dark);
+  color: white;
+  font-family: system-ui;
+}
+
+.todos {
+  display: flex;
+  background-color: rgb(0,0,0,0.75);
+  padding: 15px;
+  color: var(--light);
+  align-items: center;
+  justify-content: space-between;
+}
+
+.todoSection {
+  margin: 7%;
+  margin-top: 3%;
+}
+
+hr {
+  margin: 20px 0;
 }
 </style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,71 @@
+<template>
+    <div @click="toggleTodo()" class="todoItem"
+     :class="{'done': this.todo.done}">
+        <h2>{{ todo.todoName }}</h2>
+        <button @click="deleteTodo"><img src="../assets/bin.png" alt="Löschen"></button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ToDo",
+    props: ["todoprop", "todoindex"],
+    data() {
+        return {
+            todo: this.todoprop,
+        }
+    },
+    methods: {
+        toggleTodo() {
+            this.$emit("toggledone-index", this.todoindex);
+        },
+        deleteTodo() {
+            this.$emit("deletetodo-index", this.todoindex);
+        }
+    },
+}
+</script>
+
+<style>
+.todoItem {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 15px auto;
+    margin-bottom: 25px;
+    padding: 25px;
+    background-color: rgb(37, 37, 37);
+    border-radius: 10px;
+    box-shadow: 0 10px rgb(15, 15, 15);
+
+    cursor: pointer;
+
+    user-select: none;
+}
+
+.todoItem button {
+    all: unset;
+
+    background-color: rgba(0, 0, 0, 0.25);
+    color: white;
+    scale: 1/1;
+    padding: 10px;
+    border-radius: 5px;
+    transition-duration: 150ms;
+}
+
+.todoItem button:hover {
+    background-color: rgba(0, 0, 0, 0.5);
+}
+
+.todoItem img {
+    width: 50px;
+}
+
+.done {
+    background-color: var(--active);
+    color: black;
+    box-shadow: 0 10px rgb(35, 94, 70);
+}
+
+</style>
